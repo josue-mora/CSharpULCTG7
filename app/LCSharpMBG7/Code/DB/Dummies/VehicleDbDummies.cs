a2591f559b5e9ab74117a21a8bffe363001f0f29
josue-mora <josuemv1@hotmail.com>
2025-04-22 21:08:22 -0600

    Carrusel re made / Fixes in Vehicle dummies

--- app/LCSharpMBG7/Code/DB/Dummies/VehicleDbDummies.cs
+++ app/LCSharpMBG7/Code/DB/Dummies/VehicleDbDummies.cs
@@ -8,14 +8,14 @@ namespace LCSharpMBG7.Code.DB.Dummies
         {
             //return new List<VehicleModel>();
             List<VehicleModel> vehicleModels = new List<VehicleModel>();
-            vehicleModels.Add(new VehicleModel("Coupe", "CLA", 2025, 0, "NEW", "Auto blanco", "cla_amg.png", false));
-            vehicleModels.Add(new VehicleModel("Coupe", "GLE Coupe", 2025, 0, "NEW", "Auto blanco", "gle_coupe_amg.png", false));
-            vehicleModels.Add(new VehicleModel("Roadster", "Mercedes AMG SL", 2025, 0, "NEW", "Auto blanco", "roadster_amg_sl.png", false));
+            vehicleModels.Add(new VehicleModel("Coupé", "CLA", 2025, 0, "NEW", "Auto blanco", "cla_amg.png", false));
+            vehicleModels.Add(new VehicleModel("Coupé", "GLE Coupé", 2025, 0, "NEW", "Auto blanco", "gle_coupe_amg.png", false));
+            vehicleModels.Add(new VehicleModel("Hatchback", "Mercedes AMG SL", 2025, 0, "NEW", "Auto blanco", "roadster_amg_sl.png", false));
             vehicleModels.Add(new VehicleModel("Hatchback", "Clase A Hatchback", 2025, 0, "NEW", "Auto blanco", "clase_a_hatchback.png", false));
-            vehicleModels.Add(new VehicleModel("Sedan", "EQS Sedan", 2025, 0, "NEW", "Auto blanco", "eqs_sedan.png", false));
-            vehicleModels.Add(new VehicleModel("Sedan", "Mercedes AMG Clase A Sedan", 2025, 0, "NEW", "Auto blanco", "clase_a_sedan.png", false));
-            vehicleModels.Add(new VehicleModel("Sedan", "Clase C Sedan", 2025, 0, "NEW", "Auto blanco", "clase_c_sedan.png", false));
-            vehicleModels.Add(new VehicleModel("Sedan", "Clase E Sedan", 2025, 0, "NEW", "Auto blanco", "clase_e_sedan.png", false));
+            vehicleModels.Add(new VehicleModel("Sedán", "EQS Sedán", 2025, 0, "NEW", "Auto blanco", "eqs_sedan.png", false));
+            vehicleModels.Add(new VehicleModel("Sedán", "Mercedes AMG Clase A Sedan", 2025, 0, "NEW", "Auto blanco", "clase_a_sedan.png", false));
+            vehicleModels.Add(new VehicleModel("Sedán", "Clase C Sedán", 2025, 0, "NEW", "Auto blanco", "clase_c_sedan.png", false));
+            vehicleModels.Add(new VehicleModel("Sedán", "Clase E Sedán", 2025, 0, "NEW", "Auto blanco", "clase_e_sedan.png", false));
             vehicleModels.Add(new VehicleModel("SUV", "GLC SUV", 2025, 0, "NEW", "Auto blanco", "glc_suv.png", false));
             vehicleModels.Add(new VehicleModel("SUV", "EQE SUV", 2025, 0, "NEW", "Auto blanco", "eqe.png", false));
             vehicleModels.Add(new VehicleModel("SUV", "GLA SUV", 2025, 0, "NEW", "Auto blanco", "gla_suv.png", false));
--- app/LCSharpMBG7/Views/CarruselAutosPage.xaml.cs
+++ app/LCSharpMBG7/Views/CarruselAutosPage.xaml.cs
@@ -1,18 +1,108 @@
 using LCSharpMBG7.Code.DB;
+using LCSharpMBG7.Code.Helpers;
+using LCSharpMBG7.Code.Logical;
+using LCSharpMBG7.Code.Models;
+using System.Diagnostics;
 
 namespace LCSharpMBG7.Views;
 
 public partial class CarruselAutosPage : ContentPage
 {
-	public CarruselAutosPage()
-	{
-		InitializeComponent();
+
+    private List<VehicleModel> filteredVehicles;
+    private int index = 0;
+
+    public CarruselAutosPage()
+    {
+        InitializeComponent();
         LoadDataAsync();
+        DebugMemory.Data();
+        // Default all vehicles are filtered.
+        filteredVehicles = State.vehicles;
+        ProcessView();
+    }
+
+    private void ProcessView(int index_action = -2)
+    {
+        // Validates if there are vehicles.
+        if (filteredVehicles.Count == 0) return;
+
+        // Validates if there is only one vehicle so process is cancelled.
+        if (filteredVehicles.Count == 1) return;
+
+        // Decrease or lower index of vehicle in filtered vehicles list.
+        if (index_action == 0) index++;
+        else if (index_action == -1) index--;
+
+        // Validate if index is not out of bounds.
+        // If so, it means it circles the list.
+        if (index < 0) index = filteredVehicles.Count - 1;
+        if (index >= filteredVehicles.Count) index = 0;
+
+        // Process vehicle to be shown.
+        VehicleImage.Source = filteredVehicles[index].ImageCarousel;
+        VehicleName.Text = filteredVehicles[index].Name;
+    }
+
+    private void ProcessFilter(string _filter)
+    {
+        if (_filter == "all") filteredVehicles = State.vehicles;
+        else
+        {
+            filteredVehicles = State.vehicles.Where(vehicle => vehicle.Model == _filter).ToList();
+        }
+        ProcessView();
     }
 
     private async void LoadDataAsync()
     {
-        // Load data from data source.
-        await InitializeDataLoad.LoadAllData();
+        if (State.vehicles == null || State.vehicles.Count == 0)
+            await InitializeDataLoad.LoadAllData();
+    }
+
+    private void BTN_RIGHT(object sender, EventArgs e)
+    {
+        ProcessView(-1);
+    }
+
+    private void BTN_LEFT(object sender, EventArgs e)
+    {
+        ProcessView(0);
+    }
+
+    private void Button_Clicked_All(object sender, EventArgs e)
+    {
+        Debug.WriteLine("all");
+        ProcessFilter("all");
+    }
+
+    private void Button_Clicked_Hatchback(object sender, EventArgs e)
+    {
+        Debug.WriteLine(Constants.VEHICLE_MODELS[3]);
+        ProcessFilter(Constants.VEHICLE_MODELS[3]);
+    }
+
+    private void Button_Clicked_Sedan(object sender, EventArgs e)
+    {
+        Debug.WriteLine(Constants.VEHICLE_MODELS[1]);
+        ProcessFilter(Constants.VEHICLE_MODELS[1]);
+    }
+
+    private void Button_Clicked_SUV(object sender, EventArgs e)
+    {
+        Debug.WriteLine(Constants.VEHICLE_MODELS[0]);
+        ProcessFilter(Constants.VEHICLE_MODELS[0]);
+    }
+
+    private void Button_Clicked_Coupe(object sender, EventArgs e)
+    {
+        Debug.WriteLine(Constants.VEHICLE_MODELS[2]);
+        ProcessFilter(Constants.VEHICLE_MODELS[2]);
+    }
+
+    private async void Button_Clicked_Menu_Principal(object sender, EventArgs e)
+    {
+        //await Task.Delay(400);
+        await Shell.Current.GoToAsync("..");
     }
 }
